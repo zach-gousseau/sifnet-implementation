871da70bdd0b96cdb376a65ae07e6051858f57c7
Zacharie Gousseau <zgoussea@cedar1.cedar.computecanada.ca>
2023-02-02 15:10:47 -0800

    commit rewrite

---
+++ glorys12/motu.sh
@@ -0,0 +1,15 @@
+motuclient/motuclient.py \
+    --user zgousseau \
+    --pwd  \
+    --verbose \
+    --service-id GLOBAL_MULTIYEAR_PHY_001_030-TDS \
+    --product-id cmems_mod_glo_phy_my_0.083_P1D-m \
+    --motu http://cmems.isac.cnr.it/mis-gateway-servlet/Motu \
+    --latitude-min 50 \
+    --out-dir /home/zgoussea/scratch/ \
+    --out-name glorys12.nc \
+    --size \
+    --latitude-max 51 \
+    --longitude-min 0 \
+    --longitude-max 1 \
+    \
