87d45ead6d0be7c22127b7b9ccd6af193747f343
Anne <annec.adriano@gmail.com>
2023-02-16 13:28:39 -0800

    Changed file paths

--- submit_all.sh
+++ submit_all.sh
@@ -1,18 +1,18 @@
 for i in {1..12}
 do
-  OUTPUT="/home/zgoussea/scratch/logs/output_direct_$i.out"
+  OUTPUT="/home/aadriano/scratch/logs/output_direct_$i.out"
   echo $OUTPUT
   sh submit.sh $OUTPUT $i 0 "direct" | sbatch
 done
 
 for i in {1..12}
 do
-  OUTPUT="/home/zgoussea/scratch/logs/output_fluxes_$i.out"
+  OUTPUT="/home/aadriano/scratch/logs/output_fluxes_$i.out"
   echo $OUTPUT
   sh submit.sh $OUTPUT $i 1 "fluxes" | sbatch
 done
 
 : '
-for i in {1..12}; do tail -1 /home/zgoussea/scratch/logs/output_fluxes_$i.out; done
-for i in {1..12}; do tail -1 /home/zgoussea/scratch/logs/output_direct_$i.out; done
+for i in {1..12}; do tail -1 /home/aadriano/scratch/logs/output_fluxes_$i.out; done
+for i in {1..12}; do tail -1 /home/aadriano/scratch/logs/output_direct_$i.out; done
 '
--- submit_job.sh
+++ submit_job.sh
@@ -3,8 +3,8 @@
 #SBATCH --gres=gpu:v100l               # Request a single V100 GPU 
 #SBATCH --mem=128000                   # Request 128GB of memory
 #SBATCH --time=00-03:00                # Request a 3-hour allocation (DD-HH:MM)
-#SBATCH --output=~/projects/def-ka3scott/aadriano/test_run.log   # File in which logs will be written
+#SBATCH --output=/home/aadriano/scratch/test_run.log   # File in which logs will be written
 
 module load cuda cudnn python/3.8
-source ~/sifnet/bin/activate
-python ~/projects/def-ka3scott/aadriano/sifnet-implementation/main.py --month 0 --predict-fluxes 1 --suffix test
+source /home/aadriano/sifnet/bin/activate
+python /home/aadriano/projects/def-ka3scott/aadriano/sifnet-implementation/main.py --month 0 --predict-fluxes 1 --suffix test
