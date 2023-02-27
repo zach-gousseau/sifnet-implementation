a43d2b3a218c8fc19d7c7b92fa6ef33e97fbc0d9
Anne <annec.adriano@gmail.com>
2023-02-27 09:19:35 -0800

    Increased memory and time

--- submit_job.sh
+++ submit_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-ka3scott
 #SBATCH --gres=gpu:v100l               # Request a single V100 GPU 
-#SBATCH --mem=128000                   # Request 128GB of memory
-#SBATCH --time=00-03:00                # Request a 3-hour allocation (DD-HH:MM)
+#SBATCH --mem=187000                   # Request 128GB of memory
+#SBATCH --time=00-12:00                # Request a 3-hour allocation (DD-HH:MM)
 #SBATCH --output=/home/aadriano/scratch/test_run.log   # File in which logs will be written
 
 module load cuda cudnn python/3.8
 source /home/aadriano/sifnet/bin/activate
-python /home/aadriano/projects/def-ka3scott/aadriano/sifnet-implementation/main.py --month 0 --predict-fluxes 1 --suffix test
+python /home/aadriano/projects/def-ka3scott/aadriano/sifnet-implementation/main.py --month 1 --predict-fluxes 1 --suffix test
